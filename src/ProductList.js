0344604cab8899c80ee54c7c3a58efe6571664b4
Fabio Coradini <fabiosrc@gmail.com>
2022-07-19 17:29:15 -0300

    adding files

---
+++ src/ProductList.js
@@ -0,0 +1,19 @@
+﻿const products = [
+  {
+    emoji: "🍦",
+    name: "ice cream",
+    price: 5,
+  },
+  {
+    emoji: "🍩",
+    name: "donuts",
+    price: 2.5,
+  },
+  {
+    emoji: "🍉",
+    name: "watermelon",
+    price: 4,
+  },
+];
+
+export { products };
---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+﻿import React from "react";
+import ProductAsClass from "../Product/ProductAsClass";
+import ProductAsFunction from "../Product/ProductAsFunction";
+
+function App() {
+  return (
+    <React.Fragment>
+      <ProductAsClass />
+      <hr />
+      <ProductAsFunction />
+    </React.Fragment>
+  );
+}
+
+export default App;
